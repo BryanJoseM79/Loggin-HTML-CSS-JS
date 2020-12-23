bf98cbb1f559c15a1f3e84998035106011624abe
BryanJoseM79 <bryanjosem79@gmail.com>
2020-12-23 19:22:32 -0300

    Funcionalidad JS a medio terminar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,8 @@
-document.getElementById("btn__registrarse").addEventListener("click", register)
+document.getElementById("btn__registrarse").addEventListener("click", register);
+document.getElementById("btn__iniciar-sesion").addEventListener("click", iniciarSesion);
+
+// le decimos que las funciones se vayan activando a medida que vayamos haciendo resize en la pagina 
+window.addEventListener("resize", anchoPagina);
 
 
 // Declaramos las variables
@@ -8,15 +12,56 @@ let formulario_register = document.querySelector(".formulario__register");
 let caja_trasera_login = document.querySelector(".caja__trasera-login");
 let caja_trasera_register = document.querySelector(".caja__trasera-register");
 
+// Ajustamos el resise 
+function anchoPagina(){
+    if(window.innerWidth > 850){
+        caja_trasera_login.style.display = "block";
+        caja_trasera_register.style.display = "block";
+    }else{
+        caja_trasera_register.style.display = "block";
+        caja_trasera_register.style.opacity = "1";
+        caja_trasera_login.style.display = "none";
+        formulario_login.style.display = "block";
+        formulario_register.style.display = "none";
+        contenedor_login_register.style.left = "0px";
+    }
+}
 
 
+function iniciarSesion(){
+    if(window.innerWidth > 850){
+        formulario_register.style.display = "none";
+        contenedor_login_register.style.left = "10px";
+        formulario_login.style.display = "block";
+        caja_trasera_register.style.opacity = "1";
+        caja_trasera_login.style.opacity = "0";
 
+    }else{
+        formulario_register.style.display = "none";
+        contenedor_login_register.style.left = "0px";
+        formulario_login.style.display = "block";
+        caja_trasera_register.style.display = "block";
+        caja_trasera_login.style.display= "none";
+    }
+   
+}
 
 // Funcion para que cuando oprima el boton de registrar se mueva hacia el lado 
 function register(){
+    if(window.innerWidth > 850){
+        formulario_register.style.display = "block";
+        contenedor_login_register.style.left = "410px";
+        formulario_login.style.display = "none";
+        caja_trasera_register.style.opacity = "0";
+        caja_trasera_login.style.opacity = "1";
+    }else{
+  
     formulario_register.style.display = "block";
-    contenedor_login_register.style.left = "410px";
-    formulario_login.style.display = "none";
-    caja_trasera_register.style.opacity = "0";
-    caja_trasera_login.style.opacity = "1";
+        contenedor_login_register.style.left = "0px";
+        formulario_login.style.display = "none";
+        caja_trasera_register.style.display = "none";
+        caja_trasera_login.style.display= "block";
+        caja_trasera_login.style.opacity= "1";
+    }
+    
 }
